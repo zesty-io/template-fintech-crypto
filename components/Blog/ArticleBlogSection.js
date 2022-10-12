0ac286c41bda12251a4c8cb5c402a6c2df9ccc2a
Jomar Montuya <jomar.montuya@outlook.com>
2022-10-12 22:29:36 +0800

    update

--- components/Blog/ArticleBlogSection.js
+++ components/Blog/ArticleBlogSection.js
@@ -1,78 +1,65 @@
-const fetch = require('node-fetch');
-const configs  = require('../../zesty.config.json');
-import {React, useState, useEffect} from 'react';
+// const fetch = require('node-fetch');
+const configs = require('../../zesty.config.json');
+import { React, useState, useEffect } from 'react';
 
 const stageURL = configs.stage;
 
+const ArticleBlogSection = ({ articles }) => {
+  //   const [articles, setArticles] = useState([]);
+  //   async function fetchData(url) {
+  //     console.log('fetching data from: ', url);
+  //     let res = await fetch(url);
+  //     let data = await res.json();
+  //     setArticles(data);
+  //   }
 
-const ArticleBlogSection = () => {
-   
-    const [articles,setArticles] = useState([]);
-    async function fetchData(url) {
-        console.log('fetching data from: ', url);
-        let res = await fetch(url);
-        let data = await res.json();
-        setArticles(data);
-    } 
-
-    useEffect(() => {
-               // fetch request
-        let data = fetchData(`${stageURL}/allarticles.json?zpw=e36863`);
-        
-    },[]);   
-
+  //   useEffect(() => {
+  //     // fetch request
+  //     let data = fetchData(`${stageURL}/allarticles.json?zpw=e36863`);
+  //   }, []);
 
   return (
     <>
-
-      {
-            articles.map((article, index) => {
-
-                return (
-                    <div className="row" key={index}>
-                        <div className="col-md-6">
-                            <article className="blog-meta-three mb-60 lg-mb-40 wow fadeInUp">
-                            <figure className="post-img m0">
-                                <a href="blog-details.html" className="w-100 d-block">
-                                <img
-                                    src={article.image}
-                                    data-src="images/blog/blog_img_20.jpg"
-                                    alt=""
-                                    className="lazy-img w-100 tran4s"
-                                />
-                                </a>
-                            </figure>
-                            <div className="post-data mt-30">
-                                <div className="post-date opacity-75 text-uppercase">
-                                {article.date}
-                                </div>
-                                <a href="blog-details.html" className="mt-10 mb-15">
-                                <h4 className="tran3s blog-title fw-normal tx-dark">
-                                    {article.title}
-                                    
-                                </h4>
-                                </a>
-                                <div>
-                                <a
-                                    href="blog-details.html"
-                                    className="read-btn-two fw-500 tran3s"
-                                >
-                                    Read More
-                                </a>
-                                </div>
-                            </div>
-                            </article>
-                        </div>
-                    </div>
-                
-                );
-            })
-        
-        }
+      {articles.map((article, index) => {
+        return (
+          <div className="row" key={index}>
+            <div className="col-md-6">
+              <article className="blog-meta-three mb-60 lg-mb-40 wow fadeInUp">
+                <figure className="post-img m0">
+                  <a href={article.meta.web.uri} className="w-100 d-block">
+                    <img
+                      src={article.image.data[0].url}
+                      data-src="images/blog/blog_img_20.jpg"
+                      alt=""
+                      className="lazy-img w-100 tran4s"
+                    />
+                  </a>
+                </figure>
+                <div className="post-data mt-30">
+                  <div className="post-date opacity-75 text-uppercase">
+                    {article.date}
+                  </div>
+                  <a href="blog-details.html" className="mt-10 mb-15">
+                    <h4 className="tran3s blog-title fw-normal tx-dark">
+                      {article.title}
+                    </h4>
+                  </a>
+                  <div>
+                    <a
+                      href="blog-details.html"
+                      className="read-btn-two fw-500 tran3s"
+                    >
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </div>
+        );
+      })}
     </>
   );
 };
 
 export default ArticleBlogSection;
-
-
--- components/Blog/BlogSection.js
+++ components/Blog/BlogSection.js
@@ -1,91 +1,168 @@
 import ArticleBlogSection from 'components/Blog/ArticleBlogSection';
 
-const BlogSection = ({data}) => {
-    // const article1 = content.content.top_articles.data[0];
-	
-    return (
-        <>
-           <div className="blog-section-five mt-70 lg-mt-30">
-				<div className="container">
-					<div className="border-bottom pb-130 lg-pb-60">
-						<div className="row gx-xl-5">
-							<div className="col-lg-8">
-								<div className="blog-meta-wrapper pe-xxl-5">
-									<ArticleBlogSection />
-								</div>
-								<div className="page-pagination-one pt-30">
-									<ul className="d-flex align-items-center style-none">
-										<li className="active"><a href="#">1</a></li>
-										<li><a href="#">2</a></li>
-										<li><a href="#">3</a></li>
-										<li><a href="#">4</a></li>
-										<li>...</li>
-										<li><a href="#">Last</a></li>
-										<li className="arrow"><a href="#"><i className="bi bi-arrow-right"></i></a></li>
-									</ul>
-								</div>
-							</div>
+const BlogSection = ({ content }) => {
+  // const article1 = content.content.top_articles.data[0];
+  const { articles } = content;
+  console.log(articles);
+  return (
+    <>
+      <div className="blog-section-five mt-70 lg-mt-30">
+        <div className="container">
+          <div className="border-bottom pb-130 lg-pb-60">
+            <div className="row gx-xl-5">
+              <div className="col-lg-8">
+                <div className="blog-meta-wrapper pe-xxl-5">
+                  <ArticleBlogSection articles={articles.data} />
+                </div>
+                <div className="page-pagination-one pt-30">
+                  <ul className="d-flex align-items-center style-none">
+                    <li className="active">
+                      <a href="#">1</a>
+                    </li>
+                    <li>
+                      <a href="#">2</a>
+                    </li>
+                    <li>
+                      <a href="#">3</a>
+                    </li>
+                    <li>
+                      <a href="#">4</a>
+                    </li>
+                    <li>...</li>
+                    <li>
+                      <a href="#">Last</a>
+                    </li>
+                    <li className="arrow">
+                      <a href="#">
+                        <i className="bi bi-arrow-right"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
 
-							<div className="col-lg-4 col-md-8">
-								<div className="blog-sidebar md-mt-70">
-									<div className="blog-sidebar-search mb-55 md-mb-40">
-										<form action="#">
-											<input type="text" placeholder="Search.."/>
-											<button><i className="bi bi-search"></i></button>
-										</form>
-									</div> 
-									<div className="blog-sidebar-category mb-60 md-mb-50">
-										<h4 className="sidebar-title">Category</h4>
-										<ul className="style-none">
-											<li><a href="blog-v2.html">Digital <span className="float-end">(3)</span></a></li>
-											<li><a href="blog-v1.html">Marketing <span className="float-end">(4)</span></a></li>
-											<li><a href="blog-v2.html">Design <span className="float-end">(2)</span></a></li>
-											<li><a href="blog-v3.html">WordPress <span className="float-end">(8)</span></a></li>
-											<li><a href="blog-v1.html">Plugin <span className="float-end">(5)</span></a></li>
-										</ul>
-									</div> 
+              <div className="col-lg-4 col-md-8">
+                <div className="blog-sidebar md-mt-70">
+                  <div className="blog-sidebar-search mb-55 md-mb-40">
+                    <form action="#">
+                      <input type="text" placeholder="Search.." />
+                      <button>
+                        <i className="bi bi-search"></i>
+                      </button>
+                    </form>
+                  </div>
+                  <div className="blog-sidebar-category mb-60 md-mb-50">
+                    <h4 className="sidebar-title">Category</h4>
+                    <ul className="style-none">
+                      <li>
+                        <a href="blog-v2.html">
+                          Digital <span className="float-end">(3)</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="blog-v1.html">
+                          Marketing <span className="float-end">(4)</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="blog-v2.html">
+                          Design <span className="float-end">(2)</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="blog-v3.html">
+                          WordPress <span className="float-end">(8)</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="blog-v1.html">
+                          Plugin <span className="float-end">(5)</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
 
-									<div className="sidebar-recent-news mb-60 md-mb-50">
-										<h4 className="sidebar-title">Recent News</h4>
-										<div className="news-block d-flex align-items-center pt-20 pb-20 border-top">
-											<div><img src="images/lazy.svg" data-src="images/blog/blog_img_17.jpg" alt="" className="lazy-img"/></div>
-											<div className="post ps-4">
-												<h4 className="mb-5"><a href="blog-details.html" className="title tran3s">10 days quick challange forboost visitors.</a></h4>
-												<div className="date">23 July, 2022</div>
-											</div>
-										</div>
-										<div className="news-block d-flex align-items-center pt-20 pb-20 border-top">
-											<div><img src="images/lazy.svg" data-src="images/blog/blog_img_18.jpg" alt="" className="lazy-img"/></div>
-											<div className="post ps-4">
-												<h4 className="mb-5"><a href="blog-details.html" className="title tran3s">Easy way to boost your business.</a></h4>
-												<div className="date">23 July, 2022</div>
-											</div>
-										</div>
-										<div className="news-block d-flex align-items-center pt-20 pb-20 border-top border-bottom">
-											<div><img src="images/lazy.svg" data-src="images/blog/blog_img_19.jpg" alt="" className="lazy-img"/></div>
-											<div className="post ps-4">
-												<h4 className="mb-5"><a href="blog-details.html" className="title tran3s">Introducing new tools for your design.</a></h4>
-												<div className="date">23 July, 2022</div>
-											</div>
-										</div>
-									</div>
+                  <div className="sidebar-recent-news mb-60 md-mb-50">
+                    <h4 className="sidebar-title">Recent News</h4>
+                    <div className="news-block d-flex align-items-center pt-20 pb-20 border-top">
+                      <div>
+                        <img
+                          src="images/lazy.svg"
+                          data-src="images/blog/blog_img_17.jpg"
+                          alt=""
+                          className="lazy-img"
+                        />
+                      </div>
+                      <div className="post ps-4">
+                        <h4 className="mb-5">
+                          <a href="blog-details.html" className="title tran3s">
+                            10 days quick challange forboost visitors.
+                          </a>
+                        </h4>
+                        <div className="date">23 July, 2022</div>
+                      </div>
+                    </div>
+                    <div className="news-block d-flex align-items-center pt-20 pb-20 border-top">
+                      <div>
+                        <img
+                          src="images/lazy.svg"
+                          data-src="images/blog/blog_img_18.jpg"
+                          alt=""
+                          className="lazy-img"
+                        />
+                      </div>
+                      <div className="post ps-4">
+                        <h4 className="mb-5">
+                          <a href="blog-details.html" className="title tran3s">
+                            Easy way to boost your business.
+                          </a>
+                        </h4>
+                        <div className="date">23 July, 2022</div>
+                      </div>
+                    </div>
+                    <div className="news-block d-flex align-items-center pt-20 pb-20 border-top border-bottom">
+                      <div>
+                        <img
+                          src="images/lazy.svg"
+                          data-src="images/blog/blog_img_19.jpg"
+                          alt=""
+                          className="lazy-img"
+                        />
+                      </div>
+                      <div className="post ps-4">
+                        <h4 className="mb-5">
+                          <a href="blog-details.html" className="title tran3s">
+                            Introducing new tools for your design.
+                          </a>
+                        </h4>
+                        <div className="date">23 July, 2022</div>
+                      </div>
+                    </div>
+                  </div>
 
-									<div className="sidebar-banner-add" style={{backgroundImage:'url(images/blog/ad-bg.jpg)'}}>
-										<div className="banner-content">
-											<h4>Banner Advertise <br/>Heading</h4>
-											<p>From its medieval origins digital</p>
-											<a href="#" className="btn-twentyOne fw-500 tran3s">Download</a>
-										</div>
-									</div>
-								</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-        </>
-    );
-}
+                  <div
+                    className="sidebar-banner-add"
+                    style={{ backgroundImage: 'url(images/blog/ad-bg.jpg)' }}
+                  >
+                    <div className="banner-content">
+                      <h4>
+                        Banner Advertise <br />
+                        Heading
+                      </h4>
+                      <p>From its medieval origins digital</p>
+                      <a href="#" className="btn-twentyOne fw-500 tran3s">
+                        Download
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default BlogSection;
-
